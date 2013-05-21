66f65399d7f41f5a666032a763b94bc8aee12340
Jack Hanford <jackhanford@gmail.com>
2013-05-21 09:25:30 -0700

    Dummy form removed

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,7 +5,7 @@ class Doctor < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   has_many :availabilities
+  has_many :availabilities
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
